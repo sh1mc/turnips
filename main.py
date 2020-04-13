057a6c7fff03bf696d40a7a4bd038e470683ff05
sh1mc <s1911340@s.tsukuba.ac.jp>
2020-04-13 16:50:41 +0900

    update

--- main.py
+++ main.py
@@ -23,36 +23,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('午') or message.content.startswith('買値'):
-        ampm = ''
-        if message.content.startswith('午前') or message.content.startswith('買値'):
-            ampm = 'am'
-        elif message.content.startswith('午後'):
-            ampm = 'pm'
-        else:
-            return
-        result = re.search('[1-9][0-9]{1,2}', message.content)
-        price = None
-        if result:
-            price = (int)(result.group())
-            today = (str)(datetime.date.today())
-            data = {}
-            datapath = './data/' + today + '.json'
-            if os.path.exists(datapath):
-                with open(datapath, 'r') as f:
-                    data = json.load(f)
-            data['day'] = datetime.date.today().weekday()
-            if message.author.name in data:
-                data[message.author.name][ampm] = price
-            else:
-                data[message.author.name] = {ampm : price}
-            with open(datapath, 'w') as f:
-                json.dump(data, f, indent=4, ensure_ascii=False)
-            await message.channel.send('おｋ')
-        else:
-            await message.channel.send('ダメです')
-    
-    if message.content.startswith('グラフ'):
+    if message.content.startswith('全部見せて'):
         graph.graph()
         await message.channel.send(file=discord.File('./img/a.png'))
 
