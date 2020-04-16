5083bb7dbdf6b0b4d63a7dc8f54d1b50ad4b476f
sh1mc <s1911340@s.tsukuba.ac.jp>
2020-04-16 19:24:52 +0900

    update

--- graph.py
+++ graph.py
@@ -25,50 +25,52 @@ def graph(weeksago):
     if weeksago is not None:
         today -= datetime.timedelta(days=weeksago*7)
         today += datetime.timedelta(days=6 - (today.weekday() + 1) % 7)
-    data = []
-    days = (today.weekday() + 1) % 7 + 1
-    for i in range(days):
-        td = datetime.timedelta(days=i)
-        path = './data/' + (str)(today - td) + '.json'
-        d = {}
-        childpath = [
-            '../../turnips/data/' + (str)(today - td) + '.json',
-            '../../bei/turnips/data/' + (str)(today - td) + '.json',
-            '../../shim/turnips/data/' + (str)(today - td) + '.json'
-        ]
-        for c in childpath:
-            if os.path.exists(c):
-                with open(c) as f:
-                    d.update(json.load(f))
-        print(d)
-        with open(path, 'w') as f:
-            json.dump(d, f, indent=4, ensure_ascii=False)
-        data.append(d)
-    data.reverse()
-    print(data)
-    names = set()
-    for d in data:
-        for k in d:
-            names.add(k)
-    names.discard('day')
-    for n in names:
-        i = 0
-        y = []
-        x = []
+    for i in range(weeksago + 1)
+        data = []
+        days = (today.weekday() + 1) % 7 + 1
+        for i in range(days):
+            td = datetime.timedelta(days=i)
+            path = './data/' + (str)(today - td) + '.json'
+            d = {}
+            childpath = [
+                '../../turnips/data/' + (str)(today - td) + '.json',
+                '../../bei/turnips/data/' + (str)(today - td) + '.json',
+                '../../shim/turnips/data/' + (str)(today - td) + '.json'
+            ]
+            for c in childpath:
+                if os.path.exists(c):
+                    with open(c) as f:
+                        d.update(json.load(f))
+            print(d)
+            with open(path, 'w') as f:
+                json.dump(d, f, indent=4, ensure_ascii=False)
+            data.append(d)
+        data.reverse()
+        print(data)
+        names = set()
         for d in data:
-            if n in d:
-                i += 1
-                if 'am' in d[n]:
-                    y.append(d[n]['am'])
-                    x.append(i)
-                i += 1
-                if 'pm' in d[n]:
-                    y.append(d[n]['pm'])
-                    x.append(i)
-            else:
-                i += 2
-        print(y)
-        ax.plot(x, y, label=n, marker='o') 
-    # ax.legend()
+            for k in d:
+                names.add(k)
+        names.discard('day')
+        for n in names:
+            i = 0
+            y = []
+            x = []
+            for d in data:
+                if n in d:
+                    i += 1
+                    if 'am' in d[n]:
+                        y.append(d[n]['am'])
+                        x.append(i)
+                    i += 1
+                    if 'pm' in d[n]:
+                        y.append(d[n]['pm'])
+                        x.append(i)
+                else:
+                    i += 2
+            print(y)
+            ax.plot(x, y, label=n, marker='o') 
+            # ax.legend()
+            today += datetime.timedelta(days=7)
     plt.savefig("./img/a.png", dpi=300)
     plt.clf()
